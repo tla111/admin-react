02e2e6ed9468be4479957d41c0ecb5e60f3ebad5
Tim La <tlamarketing111@gmail.com>
2022-12-28 17:35:54 -0500

    imported components & useStateContext

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,6 +6,10 @@ import { RiNotification3Line } from 'react-icons/ri';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
+import avatar from '../data/avatar.jpg';
+import { Cart, Chat, Notification, UserProfile } from '.';
+import { useStateContext } from '../contexts/ContextProvider';
+
 const Navbar = () => {
     return (
         <div>Navbar</div>
