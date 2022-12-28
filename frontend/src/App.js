685718ecf8037ba45b1dc988821ea68696fb6d1e
Tim La <tlamarketing111@gmail.com>
2022-12-28 16:52:12 -0500

    imported components & pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 import './App.css';
 
 const App = () => {
@@ -29,11 +31,11 @@ const App = () => {
 
                     {activeMenu ? (
                         <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
-                            Sidebar
+                            <Sidebar />
                         </div>
                     ) : (
                         <div className="w-0 dark:bg-secondary-dark-bg">
-                            Sidebar w-0
+                            <Sidebar />
                         </div>
                     )}
 
