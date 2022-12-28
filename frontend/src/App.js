ba74bff564ca3e742472d7f885edd6df457ddcad
Tim La <tlamarketing111@gmail.com>
2022-12-28 16:36:10 -0500

    building sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css';
 
 const App = () => {
+    const activeMenu = true;
+
     return (
         <div>
             <BrowserRouter>
@@ -17,7 +19,6 @@ const App = () => {
                         >
                             <button
                                 type="button"
-
                                 style={{ background: "blue", borderRadius: '50%' }}
                                 className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
                             >
@@ -25,9 +26,20 @@ const App = () => {
                             </button>
                         </TooltipComponent>
                     </div>
+
+                    {activeMenu ? (
+                        <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+                            Sidebar
+                        </div>
+                    ) : (
+                        <div className="w-0 dark:bg-secondary-dark-bg">
+                            Sidebar w-0
+                        </div>
+                    )}
+
                 </div>
             </BrowserRouter>
-        </div >
+        </div>
     )
 }
 
