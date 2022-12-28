e5615390caaf763dcb17ff85042a0d5945975c2a
Tim La <tlamarketing111@gmail.com>
2022-12-28 16:39:13 -0500

    styled dib for navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -44,6 +44,9 @@ const App = () => {
                                 : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
                         }
                     >
+                        <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+                            Navbar
+                        </div>
 
                     </div>
 
