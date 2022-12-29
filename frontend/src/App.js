6388a85234126aec4e856468d7d43853e83d3623
Tim La <tlamarketing111@gmail.com>
2022-12-29 05:24:13 -0500

    building layout for ecommerce

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,14 +23,15 @@ const App = () => {
                         >
                             <button
                                 type="button"
-                                style={{ background: "blue", borderRadius: '50%' }}
+                                onClick={() => { }}
+                                style={{ background: "white", borderRadius: '50%' }}
                                 className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
                             >
                                 <FiSettings />
                             </button>
+
                         </TooltipComponent>
                     </div>
-
                     {activeMenu ? (
                         <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
                             <Sidebar />
@@ -40,7 +41,6 @@ const App = () => {
                             <Sidebar />
                         </div>
                     )}
-
                     <div
                         className={
                             activeMenu
@@ -51,37 +51,39 @@ const App = () => {
                         <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
                             <Navbar />
                         </div>
+                        <div>
+
+
+                            <Routes>
+                                {/* dashboard  */}
+                                <Route path="/" element={(<Ecommerce />)} />
+                                <Route path="/ecommerce" element={(<Ecommerce />)} />
+
+                                {/* pages  */}
+                                <Route path="/orders" element={<Orders />} />
+                                <Route path="/employees" element={<Employees />} />
+                                <Route path="/customers" element={<Customers />} />
+
+                                {/* apps  */}
+                                <Route path="/kanban" element={<Kanban />} />
+                                <Route path="/editor" element={<Editor />} />
+                                <Route path="/calendar" element={<Calendar />} />
+                                <Route path="/color-picker" element={<ColorPicker />} />
+
+                                {/* charts  */}
+                                <Route path="/line" element={<Line />} />
+                                <Route path="/area" element={<Area />} />
+                                <Route path="/bar" element={<Bar />} />
+                                <Route path="/pie" element={<Pie />} />
+                                <Route path="/financial" element={<Financial />} />
+                                <Route path="/color-mapping" element={<ColorMapping />} />
+                                <Route path="/pyramid" element={<Pyramid />} />
+                                <Route path="/stacked" element={<Stacked />} />
+
+                            </Routes>
+                        </div>
+                        <Footer />
                     </div>
-
-                    <div>
-                        <Routes>
-                            dashboard
-                            <Route path="/" element={<Ecommerce />} />
-                            <Route path="/ecommerce" element={<Ecommerce />} />
-
-                            {/* pages  */}
-                            <Route path="/orders" element={<Orders />} />
-                            <Route path="/employees" element={<Employees />} />
-                            <Route path="/customers" element={<Customers />} />
-
-                            {/* apps  */}
-                            <Route path="/kanban" element={<Kanban />} />
-                            <Route path="/editor" element={<Editor />} />
-                            <Route path="/calendar" element={<Calendar />} />
-                            <Route path="/color-picker" element={<ColorPicker />} />
-
-                            {/* charts  */}
-                            <Route path="/line" element={<Line />} />
-                            <Route path="/area" element={<Area />} />
-                            <Route path="/bar" element={<Bar />} />
-                            <Route path="/pie" element={<Pie />} />
-                            <Route path="/financial" element={<Financial />} />
-                            <Route path="/color-mapping" element={<ColorMapping />} />
-                            <Route path="/pyramid" element={<Pyramid />} />
-                            <Route path="/stacked" element={<Stacked />} />
-                        </Routes>
-                    </div>
-
                 </div>
             </BrowserRouter>
         </div>
--- frontend/src/pages/Ecommerce.jsx
+++ frontend/src/pages/Ecommerce.jsx
@@ -11,7 +11,18 @@ import product9 from '../data/product9.jpg'
 
 const Ecommerce = () => {
     return (
-        <div>Ecommerce</div>
+        <div className="mt-24">
+            <div className="flex flex-wrap lg:flex-nowrap justify-center ">
+                <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+                    <div className="flex justify-between items-center">
+                        <div>
+                            <p className="font-bold text-gray-400">Earnings</p>
+                            <p className="text-2xl">$63,448.78</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
