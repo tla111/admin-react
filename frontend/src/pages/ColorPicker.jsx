c830f3919c974791bee5981223a26771984bb463
Tim La <tlamarketing111@gmail.com>
2022-12-30 05:28:50 -0500

    built change function

--- frontend/src/pages/ColorPicker.jsx
+++ frontend/src/pages/ColorPicker.jsx
@@ -3,6 +3,10 @@ import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs';
 
 import { Header } from '../components';
 
+const change = (args) => {
+    document.getElementById('preview').style.backgroundColor = args.currentValue.hex;
+};
+
 const CustomColorPicker = ({ id, mode }) => <ColorPickerComponent id={id} mode={mode} modeSwitcher={false} inline showButtons={false} change={change} />;
 
 const ColorPicker = () => {
