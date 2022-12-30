a1663df1b02dd5981dcf98f8fbc1ee6d97f4b363
Tim La <tlamarketing111@gmail.com>
2022-12-30 05:28:09 -0500

    created logic for CustomColorPicker

--- frontend/src/pages/ColorPicker.jsx
+++ frontend/src/pages/ColorPicker.jsx
@@ -3,6 +3,8 @@ import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs';
 
 import { Header } from '../components';
 
+const CustomColorPicker = ({ id, mode }) => <ColorPickerComponent id={id} mode={mode} modeSwitcher={false} inline showButtons={false} change={change} />;
+
 const ColorPicker = () => {
     return (
         <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
