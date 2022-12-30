26cb31b5d62234f17cf66700edb3deb5fcd03e6c
Tim La <tlamarketing111@gmail.com>
2022-12-30 05:18:43 -0500

    calendar is appearing

--- frontend/src/pages/Calendar.jsx
+++ frontend/src/pages/Calendar.jsx
@@ -9,6 +9,12 @@ const Calendar = () => {
     return (
         <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
             <Header category="App" title="Calendar" />
+            <ScheduleComponent
+                height="650px"
+
+            >
+                <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]} />
+                /</ScheduleComponent>
         </div>
     )
 }
