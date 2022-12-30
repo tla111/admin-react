172f91881c1c589645f67e50907d457c53d3c0f7
Tim La <tlamarketing111@gmail.com>
2022-12-30 05:26:42 -0500

    building layout for colorPicker

--- frontend/src/pages/ColorPicker.jsx
+++ frontend/src/pages/ColorPicker.jsx
@@ -5,7 +5,13 @@ import { Header } from '../components';
 
 const ColorPicker = () => {
     return (
-        <div>ColorPicker</div>
+        <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+            <Header category="App" title="Color Picker" />
+            <div className="text-center">
+                <div id="preview" />
+
+            </div>
+        </div>
     )
 }
 
