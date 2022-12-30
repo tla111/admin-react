7682d61ff496fd7eb0b918fb1af38104cc4888b7
Tim La <tlamarketing111@gmail.com>
2022-12-30 05:25:57 -0500

    imported imports for ColorPicker

--- frontend/src/pages/Editor.jsx
+++ frontend/src/pages/Editor.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor';
 
 import { Header } from '../components';
-import { EditorData } from '../data/dummy'; s
+import { EditorData } from '../data/dummy';
 
 const Editor = () => {
     return (
