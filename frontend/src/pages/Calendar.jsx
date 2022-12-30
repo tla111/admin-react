7a62c7467278d229378f90b608f07c3f95a1d2f8
Tim La <tlamarketing111@gmail.com>
2022-12-30 05:17:18 -0500

    building layout for Calendar

--- frontend/src/pages/Calendar.jsx
+++ frontend/src/pages/Calendar.jsx
@@ -7,7 +7,9 @@ import { Header } from '../components';
 
 const Calendar = () => {
     return (
-        <div>Calendar</div>
+        <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+            <Header category="App" title="Calendar" />
+        </div>
     )
 }
 
